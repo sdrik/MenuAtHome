94394906987bbba715b07da2307dcd0acc4c1789
Cédric Schieli <cschieli@gmail.com>
2019-12-03 08:26:13 +0100

    Load Firebase SDK auto-configuration on startup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { AngularFireModule, FirebaseOptionsToken } from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,11 +12,13 @@ import { ConfigService } from './config.service';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule,
     AppRoutingModule
   ],
   providers: [
     ConfigService,
     { provide: APP_INITIALIZER, useFactory: (cs: ConfigService) => () => cs.load(), deps: [ConfigService], multi: true },
+    { provide: FirebaseOptionsToken, useFactory: (cs: ConfigService) => cs.firebase, deps: [ConfigService] },
   ],
   bootstrap: [AppComponent]
 })
--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -4,10 +4,29 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConfigService {
+  private _firebase = {};
 
   constructor() { }
 
+  private async loadFirebaseAutoConfig(): Promise<void> {
+    const response = await fetch('/__/firebase/init.json');
+    if (!response.ok) {
+      throw new Error('Bad status: ' + response.status);
+    }
+    this._firebase = await response.json();
+  }
+
   public async load(): Promise<boolean> {
-    return true;
+    try {
+      await this.loadFirebaseAutoConfig();
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  public get firebase() {
+    return this._firebase;
   }
 }
